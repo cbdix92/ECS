e96fb57263d93374b46a56a0b1cda1fcb1b4bcb5
cbdix92 <cbdix92@gmail.com>
2022-07-08 00:05:47 -0400

    Update

--- DataSystem/ComponentCollection.cs
+++ DataSystem/ComponentCollection.cs
@@ -7,21 +7,63 @@ namespace CMDR
     {
         private Dictionary<uint, T> _components;
 
+        private Dictionary<uint, uint> _idToIndexLookUp;
 
-        T IComponentCollection<T>.Get(uint id)
-        {
-            return _components[id];
-        }
+        private T[] components;
 
         internal ComponentCollection()
         {
             _components = new Dictionary<uint, T>();
         }
+
+        
+        #region PUBLIC_METHODS
+        public void Add(T component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(T[] components)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(uint id)
+        {
+            return _components[id];
+        }
+
+        
     }
 
     internal interface IComponentCollection<T>
     {
-        internal T Get(uint id);
+        public void Add(T component);
+
+        public void AddRange(T[] components);
+
+        public void Remove(T component);
+
+        public void Remove(uint id);
+
+        public bool Contains(uint id);
+
+        public T Get(uint id);
     }
 
     internal interface IComponentCollection : IComponentCollection<IComponent>
