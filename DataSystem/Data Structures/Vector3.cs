cf38909407dd9cfa39af8af2b49aeeda0ef6fd6e
cbdix92 <cbdix92@gmail.com>
2022-07-15 16:23:04 -0400

    Update

---
+++ DataSystem/Data Structures/Vector3.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CMDR
+{
+    public struct Vector3 : IEquatable<Vector3>
+    {
+        #region PUBLIC_MEMBERS
+
+        public float X;
+        
+        public float Y;
+
+        public float Z;
+
+        public static readonly Vector3 Up = new Vector3(0, -1, 0);
+        
+        public static readonly Vector3 Down = new Vector3(0, 1, 0);
+        
+        public static readonly Vector3 Left = new Vector3(-1, 0, 0);
+        
+        public static readonly Vector3 Right = new Vector3(1, 0, 0);
+        
+        public static readonly Vector3 Forward = new Vector3(0, 0, 1);
+        
+        public static readonly Vector3 Backward = new Vector3(0, 0, -1);
+
+        public float this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return X;
+                else if (index == 1)
+                    return Y;
+                else if (index == 2)
+                    return Z;
+                throw new IndexOutOfRangeException($"index of {index} is out of range of Vector4!");
+            }
+            set
+            {
+                if (index == 0)
+                    X = value;
+                else if (index == 1)
+                    Y = value;
+                else if (index == 2)
+                    Z = value;
+                throw new IndexOutOfRangeException($"index of {index} is out of range of Vector4!");
+            }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Vector3(float n) => (X, Y, Z) = (n, n, n);
+
+        public Vector3(float x, float y, float z) => (X, Y, Z) = (x, y, z);
+
+        #endregion
+
+        #region PUBLIC_METHODS
+
+        public float Magnitude() => MathF.Sqrt(X * X + Y * Y + Z * Z);
+
+        public void Invert() => (X, Y, Z) = (-X, -Y, -Z);
+
+        public void Normalize() => this /= Magnitude();
+
+        public Vector3 Cross(Vector3 other) => Cross(this, other);
+
+        public float Dot(Vector3 other) => Dot(this, other);
+
+        public static Vector3 Cross(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3((v1.Y * v2.Z) - (v1.Z * v2.Y),
+                               (v1.Z * v2.X) - (v1.X * v2.Z),
+                               (v1.X * v2.Y) - (v1.Y * v2.X));
+        }
+
+        public static float Dot(Vector3 v1, Vector3 v2) => v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+
+        public static Vector3 Normalize(Vector3 v) => v / v.Magnitude();
+
+        public static Vector3 Invert(Vector3 vec) => new Vector3() { X = -vec.X, Y = -vec.Y, Z = -vec.Z };
+
+        public static float Distance(Vector3 vec1, Vector3 vec2)
+        {
+            Vector3 result = vec1 - vec2;
+            return result.Magnitude();
+        }
+
+        public bool Equals([AllowNull] Vector3 other) => (X == other.X && Y == other.Y && Z == other.Z);
+
+        public float[] ToArray() => new float[] { X, Y, Z };
+
+        public override string ToString() => $"X:{X}, Y:{Y}, Z:{Z}";
+
+        #region VECTOR_OPERATORS
+        public static Vector3 operator +(Vector3 vec1, Vector3 vec2)
+        {
+            float x = vec1.X + vec2.X;
+            float y = vec1.Y + vec2.Y;
+            float z = vec1.Z + vec2.Z;
+            return new Vector3() { X = x, Y = y, Z = z };
+        }
+
+        public static Vector3 operator -(Vector3 vec1, Vector3 vec2)
+        {
+            float x = vec1.X - vec2.X;
+            float y = vec1.Y - vec2.Y;
+            float z = vec1.Z - vec2.Z;
+            return new Vector3() { X = x, Y = y, Z = z };
+        }
+
+        public static Vector3 operator *(Vector3 vec1, Vector3 vec2)
+        {
+            float x = vec1.X * vec2.X;
+            float y = vec1.Y * vec2.Y;
+            float z = vec1.Z * vec2.Z;
+            return new Vector3() { X = x, Y = y, Z = z };
+        }
+
+        public static Vector3 operator /(Vector3 vec1, Vector3 vec2)
+        {
+            float x = vec1.X / vec2.X;
+            float y = vec1.Y / vec2.Y;
+            float z = vec1.Z / vec2.Z;
+            return new Vector3() { X = x, Y = y, Z = z };
+        }
+        #endregion
+
+        #region SCALAR_OPERATORS
+        public static Vector3 operator +(Vector3 vec, float scalar)
+        {
+            Vector3 other = new Vector3(scalar);
+            return vec + other;
+        }
+
+        public static Vector3 operator -(Vector3 vec, float scalar)
+        {
+            Vector3 other = new Vector3(scalar);
+            return vec - other;
+        }
+
+        public static Vector3 operator *(Vector3 vec, float scalar)
+        {
+            Vector3 other = new Vector3(scalar);
+            return vec * other;
+        }
+
+        public static Vector3 operator /(Vector3 vec, float scalar)
+        {
+            Vector3 other = new Vector3(scalar);
+            return vec / other;
+        }
+        public static Vector3 operator +(float scalar, Vector3 vec)
+        {
+            Vector3 other = new Vector3(scalar);
+            return vec + other;
+        }
+
+        public static Vector3 operator -(float scalar, Vector3 vec)
+        {
+            Vector3 other = new Vector3(scalar);
+            return vec - other;
+        }
+
+        public static Vector3 operator *(float scalar, Vector3 vec)
+        {
+            Vector3 other = new Vector3(scalar);
+            return vec * other;
+        }
+
+        public static Vector3 operator /(float scalar, Vector3 vec)
+        {
+            Vector3 other = new Vector3(scalar);
+            return vec / other;
+        }
+        #endregion
+
+        #endregion
+    }
+}
--- DataSystem/Data Structures/Vector4.cs
+++ DataSystem/Data Structures/Vector4.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CMDR
 {
@@ -7,9 +8,170 @@ namespace CMDR
         #region PUBLIC_MEMBERS
 
         public float X;
+        
         public float Y;
+        
         public float Z;
+        
         public float W;
+
+        public float this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return X;
+                else if (index == 1)
+                    return Y;
+                else if (index == 2)
+                    return Z;
+                else if (index == 3)
+                    return W;
+                throw new IndexOutOfRangeException($"index of {index} is out of range of Vector4(XYZW)!");
+            }
+            set
+            {
+                if (index == 0)
+                    X = value;
+                else if (index == 1)
+                    Y = value;
+                else if (index == 2)
+                    Z = value;
+                else if (index == 3)
+                    W = value;
+                throw new IndexOutOfRangeException($"index of {index} is out of range of Vector4(XYZW)!");
+            }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Vector4(float n) => (X, Y, Z, W) = (n, n, n, n);
+
+        public Vector4(float x, float y, float z, float w) => (X, Y, Z, W) = (x, y, z, w);
+
+        #endregion
+
+        #region PUBLIC_METHODS
+
+        public void Invert()
+        {
+            (X, Y, Z, W) = (-X, -Y, -Z, -W);
+        }
+
+        /// <summary>
+        /// Calculate the Magnitude of this vector.
+        /// </summary>
+        /// <returns> The magnitude of this vector. </returns>
+        public float Magnitude()
+        {
+            return MathF.Sqrt(X * X + Y * Y + Z * Z + W * W);
+        }
+
+        /// <summary>
+        /// Normalize this vector. 
+        /// </summary>
+        public void Normalize()
+        {
+            float magnitude = Magnitude();
+
+            X /= magnitude;
+            
+            Y /= magnitude;
+            
+            Z /= magnitude;
+            
+            W /= magnitude;
+        }
+
+        public static float Dot(Vector4 v1, Vector4 v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z + v1.W * v2.W;
+        }
+
+        public static Vector4 Normalize(Vector4 v)
+        {
+            return v / v.Magnitude();
+        }
+
+        public static Vector4 Invert(Vector4 vec)
+        {
+            return new Vector4() { X = -vec.X, Y = -vec.Y, Z = -vec.Z, W = -vec.W };
+        }
+
+        public static float Distance(Vector4 vec1, Vector4 vec2)
+        {
+            Vector4 result = vec1 - vec2;
+            return result.Magnitude();
+        }
+
+        public float[] ToArray()
+        {
+            return new float[] { X, Y, Z, W };
+        }
+
+        public override string ToString()
+        {
+            return $"X:{X}, Y:{Y}, Z:{Z}, W:{W}";
+        }
+
+        public bool Equals([AllowNull] Vector4 other)
+        {
+            return (X == other.X && Y == other.Y && Z == other.Z && W == other.W);
+        }
+
+        #region VECTOR_OPERATORS
+        public static Vector4 operator +(Vector4 vec1, Vector4 vec2)
+        {
+            return new Vector4(vec1.X + vec2.X,
+                               vec1.Y + vec2.Y,
+                               vec1.Z + vec2.Z,
+                               vec1.W + vec2.W);
+        }
+
+        public static Vector4 operator -(Vector4 vec1, Vector4 vec2)
+        {
+            return new Vector4(vec1.X - vec2.X,
+                               vec1.Y - vec2.Y,
+                               vec1.Z - vec2.Z,
+                               vec1.W - vec2.W);
+        }
+
+        public static Vector4 operator *(Vector4 vec1, Vector4 vec2)
+        {
+            return new Vector4(vec1.X * vec2.X,
+                               vec1.Y * vec2.Y,
+                               vec1.Z * vec2.Z,
+                               vec1.W * vec2.W);
+        }
+
+        public static Vector4 operator /(Vector4 vec1, Vector4 vec2)
+        {
+            return new Vector4(vec1.X / vec2.X,
+                               vec1.Y / vec2.Y,
+                               vec1.Z / vec2.Z,
+                               vec1.W / vec2.W);
+        }
+        #endregion
+
+        #region SCALAR_OPERATORS
+        public static Vector4 operator +(Vector4 vec, float scalar) => vec + new Vector4(scalar);
+
+        public static Vector4 operator -(Vector4 vec, float scalar) => vec - new Vector4(scalar);
+
+        public static Vector4 operator *(Vector4 vec, float scalar) => vec * new Vector4(scalar);
+
+        public static Vector4 operator /(Vector4 vec, float scalar) => vec / new Vector4(scalar);
+
+        public static Vector4 operator +(float scalar, Vector4 vec) => new Vector4(scalar) + vec;
+
+        public static Vector4 operator -(float scalar, Vector4 vec) => new Vector4(scalar) - vec;
+
+        public static Vector4 operator *(float scalar, Vector4 vec) => new Vector4(scalar) * vec;
+
+        public static Vector4 operator /(float scalar, Vector4 vec) => new Vector4(scalar) /  vec;
+        #endregion
 
         #endregion
     }
