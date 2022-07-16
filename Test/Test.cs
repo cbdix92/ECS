6dc9a3b7234ccc87303778b8ea853f6be50a3ba2
cbdix92 <cbdix92@gmail.com>
2022-07-15 22:13:56 -0400

    Update

--- Test/Test.cs
+++ Test/Test.cs
@@ -7,27 +7,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            
-
-
-            Random R = new Random();
-            ulong[] GameObjects = new ulong[50];
-            for (int i = 0; i < GameObjects.Length; i++)
-            {
-                GameObjects[i] = (ulong)R.Next(int.MaxValue);
-                Console.WriteLine(GameObjects[i]);
-            }
-
-            ulong[] output = Radix.Sort(sizeof(uint) * 8, GameObjects);
-
-            for(int i = 0; i < output.Length; i++)
-            {
-                Console.WriteLine(output[i]);
-            }
-
             Console.ReadKey();
-
-
         }
     }
 }
